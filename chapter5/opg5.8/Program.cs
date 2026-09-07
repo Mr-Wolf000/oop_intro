5ef67ecacdb1ee8607f4511d6338b2b5c1fb9460
Anton Loenskov <anton.lonskov@gmail.com>
2026-09-07 22:21:42 +0200

    added opg5.8

---
+++ chapter5/opg5.8/Program.cs
@@ -0,0 +1,16 @@
+﻿double pi = 3.141592653;
+
+int r = 1;
+double radius = pi*r*r;
+Console.Write("for r = 1: ");
+Console.WriteLine(radius);
+
+r = 3;
+radius = pi*r*r;
+Console.Write("for r = 3: ");
+Console.WriteLine(radius);
+
+r = 5;
+radius = pi*r*r;
+Console.Write("for r = 5: ");
+Console.WriteLine(radius);
