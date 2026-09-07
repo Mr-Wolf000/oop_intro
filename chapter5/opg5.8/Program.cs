62d4a07723aa29da1e0fa712da51214a1317563d
Anton Loenskov <anton.lonskov@gmail.com>
2026-09-07 22:31:36 +0200

    used loop to solve opg5.8

--- chapter5/opg5.8/Program.cs
+++ chapter5/opg5.8/Program.cs
@@ -1,5 +1,11 @@
 ﻿double pi = 3.141592653;
+for (int i=1; i<=5; i+=2) {
+    Console.Write("For r = ");
+    Console.WriteLine(i);
+    Console.WriteLine(pi*i*i);
+}
 
+/*
 int r = 1;
 double radius = pi*r*r;
 Console.Write("for r = 1: ");
@@ -14,3 +20,4 @@ r = 5;
 radius = pi*r*r;
 Console.Write("for r = 5: ");
 Console.WriteLine(radius);
+*/
--- chapter5/opg5.8/obj/Debug/net10.0/opg5.8.AssemblyInfo.cs
+++ chapter5/opg5.8/obj/Debug/net10.0/opg5.8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("opg5.8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a482bd55b37471fa3bc3dbdc19e476f5fe34f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ef67ecacdb1ee8607f4511d6338b2b5c1fb9460")]
 [assembly: System.Reflection.AssemblyProductAttribute("opg5.8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("opg5.8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
