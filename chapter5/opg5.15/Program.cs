09a482bd55b37471fa3bc3dbdc19e476f5fe34f2
Anton Loenskov <anton.lonskov@gmail.com>
2026-09-07 22:09:18 +0200

    added opg5.15

---
+++ chapter5/opg5.15/Program.cs
@@ -0,0 +1,30 @@
+﻿int ada_lovelace = 36; // https://en.wikipedia.org/wiki/Ada_Lovelace
+int dennis_ritchie = 70; // https://en.wikipedia.org/wiki/Dennis_Ritchie
+int grace_hopper = 85; // https://en.wikipedia.org/wiki/Grace_Hopper
+int hedy_lamarr = 85; // https://en.wikipedia.org/wiki/Hedy_Lamarr
+int edsger_dijkstra = 72; // https://en.wikipedia.org/wiki/Edsger_W._Dijkstra
+int douglas_engelbart = 88; // https://en.wikipedia.org/wiki/Douglas_Engelbart
+
+float male_avg = (float)(dennis_ritchie + edsger_dijkstra + douglas_engelbart) / 3;
+float female_avg = (float)(ada_lovelace + grace_hopper + hedy_lamarr) / 3;
+float avg = (male_avg + female_avg) / 2;
+float diff = male_avg - female_avg;
+
+Console.Write("Average lifespan of a male computer scientist: ");
+Console.WriteLine(male_avg);
+Console.Write("Average lifespan of a female computer scientist: ");
+Console.WriteLine(female_avg);
+Console.Write("Average lifespan of a computer scientist: ");
+Console.WriteLine(avg);
+Console.Write("Males live this much longer than females: ");
+Console.WriteLine(diff);
+
+/*
+The program starts of by making variabels for the ages of some ppl as int (could also have been byte to save storage)
+The program makes vars male_avg, female_avg, avg and diff as floats (since they may end up being decimal numbers).
+male_avg is set to be all the male programers ages added together and devided by number of them (3)
+female_avg is the same but for female progamers
+avg is male_avg + female_avg / 2 this gets the total avg
+diff is the differense in male to female so male_avg - female_avg
+now it prints the values in a logical format for male_avg, female_avg, avg and diff
+*/
