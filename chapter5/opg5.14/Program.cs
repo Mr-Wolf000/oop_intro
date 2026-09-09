b20336a22cdff2b6a76f7adcb4b8ab06ca82cc15
Anton loenskov <anton.lonskov@gmail.com>
2026-09-09 15:00:13 +0200

    added opg5.14

---
+++ chapter5/opg5.14/Program.cs
@@ -0,0 +1,24 @@
+﻿double monday = 21.5;
+double tuesday = 23.7;
+double wednesday = 19.6;
+double thursday = 22.5;
+double friday = 25.3;
+double saturday = 21.7;
+double sunday = 18.9;
+
+Console.WriteLine(tuesday-monday);
+Console.WriteLine(wednesday-tuesday);
+Console.WriteLine(thursday-wednesday);
+Console.WriteLine(friday-thursday);
+Console.WriteLine(saturday-friday);
+Console.WriteLine(sunday-saturday);
+
+/*
+• Monday: 21.5
+• Tuesday: 23.7
+• Wednesday: 19.6
+• Thursday: 22.5
+• Friday: 25.3
+• Saturday: 21.7
+• Sunday: 18.9
+*/
