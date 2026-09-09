ab0f9db0b4cc54e352d0b08ee5a37c03fa88d8af
Anton loenskov <anton.lonskov@gmail.com>
2026-09-09 15:15:14 +0200

    added opg5.11

---
+++ chapter5/opg5.11/Program.cs
@@ -0,0 +1,9 @@
+﻿const int timestamp = 100000000;
+const int day_t = 24 * 60 * 60;
+const int year_t = 365 * day_t;
+int day;
+int year;
+year = timestamp/year_t;
+day = timestamp/day_t-year*365;
+Console.WriteLine(day);
+Console.WriteLine(year);
