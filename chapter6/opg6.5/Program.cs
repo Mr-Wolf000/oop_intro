342578d67c0486b941287bf324f5394a8c494f0b
Anton loenskov <anton.lonskov@gmail.com>
2026-09-14 12:49:27 +0200

    added opg6.5

--- chapter6/opg6.5/Program.cs
+++ chapter6/opg6.5/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int month = 12;
+
+switch (month) {
+    case 10:
+        Console.WriteLine("Autumn Holiday");
+        break;
+    case 12:
+        Console.WriteLine("Christmas Holiday");
+        break;
+    case 4:
+        Console.WriteLine("Spring Holiday");
+        break;
+    case 7:
+        Console.WriteLine("Summer Holiday");
+        break;
+    case 8:
+        Console.WriteLine("Summer Holiday");
+        break;
+    default:
+        Console.WriteLine("Hard Work");
+        break;
+}
