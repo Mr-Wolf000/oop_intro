a24d068eb14fd0185cec054903ec9dab3623e5ef
Anton Loenskov <anton.lonskov@gmail.com>
2026-09-15 22:02:05 +0200

    added opg7.15

---
+++ chapter7/opg7.15/Program.cs
@@ -0,0 +1,9 @@
+﻿Person person1 = new Person { name = "John", age = 22, dogsName = "Theo" };
+
+Console.WriteLine(person1.name+" is "+person1.age+" years old, and there dogs name is "+person1.dogsName);
+
+class Person {
+    public string name;
+    public int age;
+    public string dogsName;
+}
