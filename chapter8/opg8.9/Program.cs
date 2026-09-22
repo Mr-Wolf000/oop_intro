aef10d8e7f09f26e9656150d9c7a6112225903b6
Anton Loenskov <anton.lonskov@gmail.com>
2026-09-22 21:44:05 +0200

    added opg 8.9

---
+++ chapter8/opg8.9/Program.cs
@@ -0,0 +1,7 @@
+﻿const double pi = 3.1415;
+double area(int r) {
+    return r*r*pi;
+}
+for (int i = 1 ; i<=10 ; i+=2) {
+    Console.WriteLine("For a cirkle with radius: {1} the area is {0:F2}", area(i), i);
+}
