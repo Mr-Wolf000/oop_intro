f5d33e94caf8665088375ce04370c279bba9c895
Anton Loenskov <anton.lonskov@gmail.com>
2026-09-22 21:29:26 +0200

    added opg8.8

---
+++ chapter8/opg8.8/Program.cs
@@ -0,0 +1,11 @@
+﻿ulong fac(ulong n) {
+    switch(n) {
+        case 0:
+            return 0;
+        case 1:
+            return 1;
+        default:
+            return n*fac(n-1);
+    }
+}
+Console.WriteLine(fac(6));
