df38efaf5aa0b204ef29989c6ef9897a492a2184
Anton Loenskov <anton.lonskov@gmail.com>
2026-09-22 21:04:06 +0200

    added opg8.4

---
+++ chapter8/opg8.4/Program.cs
@@ -0,0 +1,11 @@
+﻿int sum(int a, int b) {
+    return a+b;
+}
+
+// Makes a table with i being the height and j being the length
+for (int i = 1 ; i<15 ; i++) {
+    for (int j = 1 ; j<10 ; j++) {
+        Console.Write("{0,3}",sum(i,j));
+    }
+    Console.WriteLine("");
+}
